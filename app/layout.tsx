1d84d03e8652905925ca2fc23e43eacfe97a08b2
Joshua Magara Onsongo <bitnboygarah@gmail.com>
2026-09-05 15:39:16 +0300

    feat: add google site verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="bg-slate-950">
+      <head>
+        <meta name="google-site-verification" content="w7PzgVHUesNwMVx4mxMmPv4n9LLejT4eGMNgJrKrJIc" />
+      </head>
       <body className="antialiased bg-slate-950 text-slate-100 min-h-screen flex flex-col">
         
         {/* GLOBAL HEADER */}
